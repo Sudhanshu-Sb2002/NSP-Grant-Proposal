3e80cea73445a5e3ab29a5735383fb3a924f05a5
Sudhanshu-Sb2002 <sudhanshuavk@gmail.com>
2022-04-26 09:20:04 +0530

    ported stuff to python

--- Python-Code/Input_function.py
+++ Python-Code/Input_function.py
@@ -5,8 +5,14 @@ import torch
 import matplotlib.pyplot as plt
 
 def import_data1(path=None):
+    numpy_unpacked=[]
     rawdata = sio.loadmat(path)
+    datamatrix=rawdata['datamatrix']
+    attributes=datamatrix[:][:11]
+    train_data=np.concatenate((datamatrix[0:550],datamatrix[-528:]),axis=0)
+    for keys,values in rawdata.items():
+        a=1
 
 
-import_data1("D:\\OneDrive - Indian Institute of Science\\4th Sem\\NSP\\NSP-Grant-Proposal\\MATLAB-COde\\tempvital.mat")
+import_data1("D:\\OneDrive - Indian Institute of Science\\4th Sem\\NSP\\NSP-Grant-Proposal\\MATLAB-COde\\forPython.mat")
 
