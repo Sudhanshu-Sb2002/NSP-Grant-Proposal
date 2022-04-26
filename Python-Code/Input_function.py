da727ff86c6e6936cdc0b42cc7da2fc322dc51fc
Sudhanshu-Sb2002 <sudhanshuavk@gmail.com>
2022-04-26 07:49:34 +0530

    temp_commit
    no real purpouse

--- Python-Code/Input_function.py
+++ Python-Code/Input_function.py
@@ -3,13 +3,10 @@ import scipy.io as sio
 import numpy as np
 import torch
 import matplotlib.pyplot as plt
-#rawdata = sio.loadmat(path)
+
 def import_data1(path=None):
-    if path is None:
-        time=np.linspace(0,20,1000)
-        signal=np.sin(time)+0.5*np.sin(time/2)+0.25*np.cos(time*2)+0.1*np.random.rand(1000)
-        return [np.array([signal]*6), np.array([time]*6)]
-    else:
-        data = sio.loadmat(path)
-        return data
+    rawdata = sio.loadmat(path)
+
+
+import_data1("D:\\OneDrive - Indian Institute of Science\\4th Sem\\NSP\\NSP-Grant-Proposal\\MATLAB-COde\\tempvital.mat")
 
