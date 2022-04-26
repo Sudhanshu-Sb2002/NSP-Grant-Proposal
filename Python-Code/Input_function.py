e490744c797c328ec597cc89073319fd7bba0e4b
Sudhanshu-Sb2002 <sudhanshuavk@gmail.com>
2022-04-26 10:55:10 +0530

    interpolation for dealing with nan values

--- Python-Code/Input_function.py
+++ Python-Code/Input_function.py
@@ -1,18 +1,31 @@
-# First we need to import matlab .mat data files uisng scipy
 import scipy.io as sio
 import numpy as np
-import torch
-import matplotlib.pyplot as plt
+
 
 def import_data1(path=None):
-    numpy_unpacked=[]
+
     rawdata = sio.loadmat(path)
-    datamatrix=rawdata['datamatrix']
-    attributes=datamatrix[:][:11]
-    train_data=np.concatenate((datamatrix[0:550],datamatrix[-528:]),axis=0)
-    for keys,values in rawdata.items():
-        a=1
+    datamatrix = rawdata['datamatrix']
+
+    for i in range(datamatrix.shape[1]):
+        datamatrix[:, i] = nan_helper(datamatrix[:, i])
+
+    train_data = np.concatenate((datamatrix[0:550], datamatrix[-528:]), axis=0)
+    test_data = datamatrix[550:721]
+
+    train_attributes = train_data[:, :11]
+    train_labels = train_data[:, 11:]
+
+    test_attributes = test_data[:, :11]
+    test_labels = test_data[:, 11:]
+
+    return [train_attributes, train_labels, test_attributes, test_labels]
+
+
+def nan_helper(y):
+    nans, x = np.isnan(y), lambda z: z.nonzero()[0]
+    y[nans] = np.interp(x(nans), x(~nans), y[~nans])
+    return y
 
 
 import_data1("D:\\OneDrive - Indian Institute of Science\\4th Sem\\NSP\\NSP-Grant-Proposal\\MATLAB-COde\\forPython.mat")
-
--- Python-Code/main.py
+++ Python-Code/main.py
@@ -2,16 +2,7 @@ from Input_function import import_data1
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.signal as sp
-[data,time]=import_data1(None)
+#[data,time]=import_data1(None)
+#import_data1("D:\\OneDrive - Indian Institute of Science\\4th Sem\\NSP\\NSP-Grant-Proposal\\MATLAB-COde\\forPython.mat")
+[train_x, train_y, test_x, test_y] = import_data1("D:\\OneDrive - Indian Institute of Science\\4th Sem\\NSP\\NSP-Grant-Proposal\\MATLAB-COde\\forPython.mat")
 
-from pyhht.visualization import plot_imfs
-from pyhht import EMD
-decomposer = EMD(data[0])
-imfs = decomposer.decompose()
-plot_imfs(data[0], imfs, time[0]);
-x=sp.hilbert(imfs[0])
-for imf in imfs[1:]:
-    x+=sp.hilbert(imf)
-
-plt.plot(x)
-plt.show()
